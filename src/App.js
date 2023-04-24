d3b12ae2ced80a951a68df1b7836b4ea0abb5516
npkuu <npkuu@naver.com>
2023-04-24 11:27:20 +0900

    이미지 넣기까지 commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button, Navbar, Container, Nav} from 'react-bootstrap';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">Kuushop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Shop</Nav.Link>
+            <Nav.Link href="#pricing">Magazine</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className='main-bg'></div>
+
+      <div className='container'>
+        <div className='row'>
+          <div className='col'>
+            <img src='https://codingapple1.github.io/shop/shoes1.jpg' width='80%'/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className='col'>
+            <img src='https://codingapple1.github.io/shop/shoes2.jpg'width='80%'/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className='col'>
+            <img src='https://codingapple1.github.io/shop/shoes3.jpg'width='80%'/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
